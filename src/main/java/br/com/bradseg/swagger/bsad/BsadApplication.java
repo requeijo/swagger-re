f0fc5ae48a0f769d131bb0223dde2c532d664e57
Andre Requeijo <requeijo@gmail.com>
2023-09-29 18:47:50 -0300

    ajustes binario

--- src/main/java/br/com/bradseg/swagger/bsad/BsadApplication.java
+++ src/main/java/br/com/bradseg/swagger/bsad/BsadApplication.java
@@ -1,7 +1,19 @@
 package br.com.bradseg.swagger.bsad;
 
+import io.swagger.v3.oas.annotations.servers.Servers;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class BsadApplication {
@@ -10,4 +22,20 @@ public class BsadApplication {
         SpringApplication.run(BsadApplication.class, args);
     }
 
+
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        Server server = new Server();
+        server.setUrl("wsphttp.bradseg.com.br");
+        return new OpenAPI()
+                .info(new Info().title("Documentação da Nova Jornada Renovação RE")
+                        .description("API para a nova Jornada de renovação RE")
+                        .version("v0.0.2")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .servers(List.of(server));
+//                .externalDocs(new ExternalDocumentation()
+//                        .description("SpringShop Wiki Documentation")
+//                        .url("http://wsphttp.dsv.bradseg.com.br"))
+
+    }
 }
--- src/main/java/br/com/bradseg/swagger/bsad/api/DisponibilizaCondicoesGeraisController.java
+++ src/main/java/br/com/bradseg/swagger/bsad/api/DisponibilizaCondicoesGeraisController.java
@@ -30,6 +30,80 @@ import java.nio.file.Paths;
 public class DisponibilizaCondicoesGeraisController {
 
 
+//    @Operation(
+//            description = "Efetua o download do documento de condições gerais",
+//            summary = "Download do documento de condições gerais",
+//            responses = {
+//                    @ApiResponse(
+//                            description = "Sucesso",
+//                            responseCode = "200",
+//                            content = @Content(mediaType = "application/json",
+//                                    schema = @Schema(type = "String", format = "byte"))
+//                    ),
+//                    @ApiResponse(
+//                            description = "Documento não encontrado",
+//                            responseCode = "404"
+//                    )
+//            }
+//
+//    )
+//    @RequestMapping(path = "/download", method = RequestMethod.GET)
+//    public ResponseEntity<Resource> download(String param) throws IOException {
+//
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+//        headers.add("Pragma", "no-cache");
+//        headers.add("Expires", "0");
+//
+//        File file = new File("");
+//
+//        Path path = Paths.get(file.getAbsolutePath());
+//        ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(path));
+//
+//        return ResponseEntity.ok()
+//                .headers(headers)
+//                .contentLength(file.length())
+//                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+//                .body(resource);
+//    }
+
+
+
+//        @Operation(
+//            description = "Efetua o download do documento de condições gerais",
+//            summary = "Download do documento de condições gerais",
+//            responses = {
+//                    @ApiResponse(
+//                            description = "Sucesso",
+//                            responseCode = "200",
+//                            content = @Content(mediaType = "application/json",
+//                                    schema = @Schema(type = "String", format = "byte"))
+//                    ),
+//                    @ApiResponse(
+//                            description = "Documento não encontrado",
+//                            responseCode = "404"
+//                    )
+//            }
+//
+//    )
+//    @Operation(summary = "Download an image.")
+//    @ApiResponses(value = {
+//            @ApiResponse(responseCode = "200",
+//                    description = "Successfully downloaded the image",
+//                    content = @Content(mediaType = "application/pdf",
+//                            schema = @Schema(type = "String", format = "binary")))
+//    })
+//    @GetMapping(value = "/download/{fileName}", produces = MediaType.APPLICATION_PDF_VALUE)
+//    public byte[] download(@PathVariable String fileName) {
+//        return new byte[1024];
+//    }
+
+
+//    @ApiResponse(
+//            content = @Content(
+//                    schema = @Schema(
+//                            type = "string", format = "binary")),
+//            responseCode = "200")
     @Operation(
             description = "Efetua o download do documento de condições gerais",
             summary = "Download do documento de condições gerais",
@@ -37,8 +111,9 @@ public class DisponibilizaCondicoesGeraisController {
                     @ApiResponse(
                             description = "Sucesso",
                             responseCode = "200",
-                            content = @Content(mediaType = "application/json",
-                                    schema = @Schema(type = "String", format = "byte"))
+                            content = @Content(
+                                    schema = @Schema(
+                                            type = "string", format = "binary"))
                     ),
                     @ApiResponse(
                             description = "Documento não encontrado",
@@ -47,25 +122,9 @@ public class DisponibilizaCondicoesGeraisController {
             }
 
     )
-    @RequestMapping(path = "/download", method = RequestMethod.GET)
-    public ResponseEntity<Resource> download(String param) throws IOException {
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
-        headers.add("Pragma", "no-cache");
-        headers.add("Expires", "0");
-
-        File file = new File("");
-
-        Path path = Paths.get(file.getAbsolutePath());
-        ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(path));
-
-        return ResponseEntity.ok()
-                .headers(headers)
-                .contentLength(file.length())
-                .contentType(MediaType.APPLICATION_OCTET_STREAM)
-                .body(resource);
+    @GetMapping(value = "/", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    public ResponseEntity<byte[]> downloadFile() {
+        return ResponseEntity.ok().body(new byte[1024]);
     }
-
 
 }
